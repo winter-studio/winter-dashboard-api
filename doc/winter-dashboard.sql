cfd36c71406151a784eccc1e0857259f0ab2f288
Kyun <me@iamkyun.com>
2022-06-09 22:22:39 +0800

    db

--- doc/winter-dashboard.sql
+++ doc/winter-dashboard.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80027
  File Encoding         : 65001
 
- Date: 29/05/2022 23:40:34
+ Date: 09/06/2022 22:22:17
 */
 
 SET NAMES utf8mb4;
@@ -27,39 +27,44 @@ CREATE TABLE `menu`  (
   `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '路径',
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '标题',
   `icon` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '图标',
-  `extra` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '额外标识',
+  `tags` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '额外标识',
   `data` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '页面组件/链接',
-  `type` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '组件类型(dir/view/link/iframe)',
+  `type` tinyint(1) NOT NULL COMMENT '组件类型(1:dir/2:view/3:link/4:iframe)',
   `hidden` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否隐藏',
   `keep_alive` tinyint(1) NOT NULL DEFAULT 1 COMMENT '是否缓存',
-  `permit_all` tinyint UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否可匿名访问',
   `sort` smallint NOT NULL COMMENT '排序(基于同级)',
   `deleted` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 39 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 45 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of menu
 -- ----------------------------
-INSERT INTO `menu` VALUES (20, NULL, 'dashboard', 'Dashboard', 'DashboardOutlined', NULL, NULL, 'dir', 0, 0, 0, 1, 0);
-INSERT INTO `menu` VALUES (21, 20, 'workplace', '工作台', NULL, NULL, 'views/dashboard/Workplace.vue', 'view', 0, 1, 0, 1, 0);
-INSERT INTO `menu` VALUES (22, NULL, 'external', '项目文档', 'DocumentTextOutline', NULL, 'https://github.com/winter-studio/winter-dashboard-ui', 'link', 0, 0, 0, 8, 0);
-INSERT INTO `menu` VALUES (23, NULL, 'exception', '异常页面', 'ExclamationCircleOutlined', NULL, NULL, 'dir', 0, 0, 0, 7, 0);
-INSERT INTO `menu` VALUES (24, 23, '403', '403', NULL, NULL, 'views/basic/exception/403.vue', 'view', 0, 1, 0, 1, 0);
-INSERT INTO `menu` VALUES (25, 23, '404', '404', NULL, NULL, 'views/basic/exception/404.vue', 'view', 0, 1, 0, 2, 0);
-INSERT INTO `menu` VALUES (26, 23, '500', '500', NULL, NULL, 'views/basic/exception/500.vue', 'view', 0, 1, 0, 3, 0);
-INSERT INTO `menu` VALUES (27, NULL, 'frame', '外部页面', 'DesktopOutline', NULL, NULL, 'dir', 0, 0, 0, 6, 0);
-INSERT INTO `menu` VALUES (28, 27, 'naive-ui', 'NaiveUi', NULL, NULL, 'https://www.naiveui.com', 'iframe', 0, 1, 0, 1, 0);
-INSERT INTO `menu` VALUES (29, NULL, 'result', '结果页面', 'CheckCircleOutlined', NULL, NULL, 'dir', 0, 0, 0, 2, 0);
-INSERT INTO `menu` VALUES (30, 29, 'success', '成功页', NULL, NULL, 'views/result/Success.vue', 'view', 0, 1, 0, 1, 0);
-INSERT INTO `menu` VALUES (31, 29, 'fail', '失败页', NULL, NULL, 'views/result/Failure.vue', 'view', 0, 1, 0, 2, 0);
-INSERT INTO `menu` VALUES (32, 29, 'info', '信息页', NULL, NULL, 'views/result/Info.vue', 'view', 0, 1, 0, 3, 0);
-INSERT INTO `menu` VALUES (33, NULL, 'setting', '设置页面', 'SettingOutlined', NULL, NULL, 'dir', 0, 0, 0, 5, 0);
-INSERT INTO `menu` VALUES (34, 33, 'account', '个人设置', NULL, NULL, 'views/setting/account/Account.vue', 'view', 0, 1, 0, 1, 0);
-INSERT INTO `menu` VALUES (35, 33, 'system', '系统设置', NULL, NULL, 'views/setting/system/System.vue', 'view', 0, 1, 0, 2, 0);
-INSERT INTO `menu` VALUES (36, NULL, 'system', '系统设置', 'OptionsSharp', NULL, NULL, 'dir', 0, 0, 0, 4, 0);
-INSERT INTO `menu` VALUES (37, 36, 'menu', '菜单权限管理', NULL, NULL, 'views/system/menu/Menu.vue', 'view', 0, 1, 0, 1, 0);
-INSERT INTO `menu` VALUES (38, NULL, 'about', '关于', 'ProjectOutlined', 'new', 'views/About.vue', 'view', 0, 1, 0, 3, 0);
+INSERT INTO `menu` VALUES (20, NULL, 'dashboard', 'Dashboard', 'DashboardOutlined', NULL, NULL, 1, 0, 0, 1, 0);
+INSERT INTO `menu` VALUES (21, 20, 'workplace', '工作台', NULL, NULL, 'views/dashboard/Workplace.vue', 2, 0, 1, 1, 0);
+INSERT INTO `menu` VALUES (22, NULL, 'external', '项目文档', 'ApiOutlined', NULL, 'https://github.com/winter-studio/winter-dashboard-ui', 3, 0, 0, 9, 0);
+INSERT INTO `menu` VALUES (23, NULL, 'exception', '异常页面', 'ExclamationCircleOutlined', NULL, NULL, 1, 0, 0, 7, 0);
+INSERT INTO `menu` VALUES (24, 23, '403', '403', NULL, NULL, 'views/basic/exception/403.vue', 2, 0, 1, 8, 0);
+INSERT INTO `menu` VALUES (25, 23, '404', '404', NULL, NULL, 'views/basic/exception/404.vue', 2, 0, 1, 9, 0);
+INSERT INTO `menu` VALUES (26, 23, '500', '500', NULL, NULL, 'views/basic/exception/500.vue', 2, 0, 1, 11, 0);
+INSERT INTO `menu` VALUES (27, NULL, 'frame', '外部页面', 'LinkOutlined', NULL, NULL, 1, 0, 0, 6, 0);
+INSERT INTO `menu` VALUES (28, 27, 'naive-ui', 'NaiveUi', 'BookOutlined', NULL, 'https://www.naiveui.com', 4, 0, 1, 1, 0);
+INSERT INTO `menu` VALUES (29, NULL, 'result', '结果页面', 'CheckCircleOutlined', NULL, NULL, 1, 0, 0, 2, 0);
+INSERT INTO `menu` VALUES (30, 29, 'success', '成功页', NULL, NULL, 'views/result/Success.vue', 2, 0, 1, 1, 0);
+INSERT INTO `menu` VALUES (31, 29, 'fail', '失败页', NULL, NULL, 'views/result/Failure.vue', 2, 0, 1, 2, 0);
+INSERT INTO `menu` VALUES (32, 29, 'info', '信息页', NULL, NULL, 'views/result/Info.vue', 2, 0, 1, 3, 0);
+INSERT INTO `menu` VALUES (33, NULL, 'setting', '设置页面', 'SettingOutlined', NULL, NULL, 1, 0, 0, 5, 0);
+INSERT INTO `menu` VALUES (34, 33, 'account', '个人设置', NULL, NULL, 'views/setting/account/Account.vue', 2, 0, 1, 1, 0);
+INSERT INTO `menu` VALUES (35, 33, 'system', '系统设置', NULL, NULL, 'views/setting/system/System.vue', 2, 0, 1, 2, 0);
+INSERT INTO `menu` VALUES (36, NULL, 'system', '系统设置', 'AppstoreAddOutlined', NULL, NULL, 1, 0, 0, 4, 0);
+INSERT INTO `menu` VALUES (37, 36, 'menu', '菜单管理', 'MenuOutlined', NULL, 'views/system/menu/Menu.vue', 2, 0, 1, 1, 0);
+INSERT INTO `menu` VALUES (38, NULL, 'about', '关于', 'ProjectOutlined', 'new', 'views/About.vue', 2, 0, 1, 3, 0);
+INSERT INTO `menu` VALUES (39, NULL, 'blog', '博客', 'BookOutlined', NULL, 'https://www.iamkyun.com', 3, 0, 1, 12, 1);
+INSERT INTO `menu` VALUES (40, NULL, 'test', '目录测试', NULL, NULL, NULL, 1, 0, 1, 13, 1);
+INSERT INTO `menu` VALUES (41, 40, 'test', '测试1', 'AimOutlined', NULL, 'https://baidu.com', 3, 0, 1, 1, 1);
+INSERT INTO `menu` VALUES (42, NULL, 'dddd', 'dddd', 'AccountBookOutlined', NULL, NULL, 1, 0, 1, 14, 1);
+INSERT INTO `menu` VALUES (43, NULL, '22', '22', NULL, NULL, NULL, 1, 0, 1, 15, 1);
+INSERT INTO `menu` VALUES (44, NULL, '123', '213', NULL, NULL, NULL, 1, 0, 1, 16, 1);
 
 -- ----------------------------
 -- Table structure for role
@@ -70,7 +75,7 @@ CREATE TABLE `role`  (
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '角色名称',
   `code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '角色代码',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of role
@@ -85,7 +90,7 @@ CREATE TABLE `role_menu`  (
   `role_id` int UNSIGNED NOT NULL,
   `menu_id` int NOT NULL,
   PRIMARY KEY (`role_id`, `menu_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of role_menu
@@ -137,7 +142,7 @@ CREATE TABLE `user_role`  (
   `user_id` bigint UNSIGNED NOT NULL COMMENT '用户ID',
   `role_id` int UNSIGNED NOT NULL,
   PRIMARY KEY (`user_id`, `role_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of user_role
