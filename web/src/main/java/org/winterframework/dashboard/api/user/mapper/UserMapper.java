7f18b2c9164cf3d5cac63754ba368e3d5b73cfb2
Kyun <me@iamkyun.com>
2022-07-10 23:03:29 +0800

    user list query

--- web/src/main/java/org/winterframework/dashboard/api/user/mapper/UserMapper.java
+++ web/src/main/java/org/winterframework/dashboard/api/user/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package org.winterframework.dashboard.api.user.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.ibatis.annotations.Param;
 import org.winterframework.dashboard.api.user.entity.User;
 import org.winterframework.dashboard.api.user.model.request.AdminUserPageReq;
 import org.winterframework.dashboard.api.user.model.response.AdminUserPageItem;
@@ -13,5 +14,5 @@ import org.winterframework.dashboard.api.user.model.response.AdminUserPageItem;
  */
 public interface UserMapper extends BaseMapper<User> {
 
-    IPage<AdminUserPageItem> pagingQueryUsers(Page<?> page, AdminUserPageReq req);
+    IPage<AdminUserPageItem> pagingQueryUsers(Page<?> page, @Param("query") AdminUserPageReq req);
 }
--- web/src/main/java/org/winterframework/dashboard/api/user/model/request/AdminUserPageReq.java
+++ web/src/main/java/org/winterframework/dashboard/api/user/model/request/AdminUserPageReq.java
@@ -1,8 +1,15 @@
 package org.winterframework.dashboard.api.user.model.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
 import org.winterframework.dashboard.web.model.PageParam;
 
 @Schema(description = "管理员用户分页查询参数")
+@Getter
+@Setter
 public class AdminUserPageReq extends PageParam {
+    private String username;
+    private String nickname;
+    private String mobile;
 }
