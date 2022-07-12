6b2627d65053d2c2909487fe99912ba37f3f9547
Kyun <me@iamkyun.com>
2022-07-12 18:07:17 +0800

    add user status

--- web/src/main/java/org/winterframework/dashboard/api/basis/controller/UploadFileController.java
+++ web/src/main/java/org/winterframework/dashboard/api/basis/controller/UploadFileController.java
@@ -1,8 +1,14 @@
 package org.winterframework.dashboard.api.basis.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.winterframework.dashboard.api.basis.model.TemporaryFileUpload;
+import org.winterframework.dashboard.web.model.ApiRes;
 
 /**
  * @author Kyun
@@ -11,7 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "文件管理模块")
 @RestController
 @RequestMapping("/files")
+@Slf4j
 public class UploadFileController {
 
+    @Operation(summary = "上传临时文件", description = "上传临时文件，返回临时文件ID")
+    @PostMapping(path = "/temp",
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ApiRes<String> uploadTemporaryFile(TemporaryFileUpload temporaryFileUpload) {
+        log.info("{}", temporaryFileUpload);
 
+        return ApiRes.success("ID");
+    }
 }
