edbb7c1b942a9d621b26e8bc16458c0341aaedad
Kyun <me@iamkyun.com>
2022-07-25 13:09:09 +0800

    optimize import

--- web/src/main/java/cn/wintersoft/dashboard/api/user/model/request/UserEditForm.java
+++ web/src/main/java/cn/wintersoft/dashboard/api/user/model/request/UserEditForm.java
@@ -3,8 +3,6 @@ package cn.wintersoft.dashboard.api.user.model.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
